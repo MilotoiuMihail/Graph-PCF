3f073717b9b6db9e50c81878f6bff69a18b3f767
Milotoiu Mihail <milotoiu.mihail1@gmail.com>
2025-07-08 13:03:01 +0300

    added JSON input

--- DemoField/index.ts
+++ DemoField/index.ts
@@ -1,18 +1,13 @@
 import { IInputs, IOutputs } from "./generated/ManifestTypes";
 
 interface GridMetrics {
-    gridLeft: number;
-    gridTop: number;
-    gridRight: number;
-    gridBottom: number;
-    gridWidth: number;
-    gridHeight: number;
-    centerX: number;
-    centerY: number;
+    gridStart: number;
+    gridEnd: number;
+    gridSize: number;
+    center: number;
     unitSize: number;
     gridStep: number;
-    axisLimitX: number;
-    axisLimitY: number;
+    axisLimit: number;
     margin: number;
 }
 
@@ -20,46 +15,30 @@ interface Circle {
     x: number,
     y: number,
     radius: number,
-    color: string,
-    dashed: boolean
+    outlineColor: string,
+    fillColor: string | null,
+    dashed: boolean,
+    zIndex: number
 }
 
 export class DemoField implements ComponentFramework.StandardControl<IInputs, IOutputs> {
 
+    private readonly DEFAULT_CANVAS_SIZE = 400;
+    private readonly MIN_CANVAS_SIZE = 200;
+    private readonly DEFAULT_AXIS_LIMIT = 1;
+
     private container: HTMLDivElement;
     private context: ComponentFramework.Context<IInputs>;
-    private canvas: HTMLCanvasElement;
-    private ctx: CanvasRenderingContext2D;
+    private gridCanvas: HTMLCanvasElement;
+    private gridCtx: CanvasRenderingContext2D;
+    private circleCanvas: HTMLCanvasElement;
+    private circleCtx: CanvasRenderingContext2D;
     private tooltip: HTMLDivElement;
     private tooltipTimeoutId: number | null = null;
 
-    private readonly DEFAULT_CANVAS_SIZE = 400;
-    private readonly MIN_CANVAS_SIZE = 200;
-    private readonly INNER_CIRCLE_FILL = "#ccc";
-    private readonly INNER_CIRCLE_OUTLINE = "#000"
-
-
-    private circles: Circle[] = [
-        // Top-left
-        { x: -.975, y: 1.7, radius: 1.35, color: "#000", dashed: false },
-        { x: -1.075, y: 1.75, radius: 1.35, color: "#f00", dashed: true },
-
-        // Top-right
-        { x: .975, y: 1.7, radius: 1.35, color: "#000", dashed: false },
-        { x: 1.075, y: 1.75, radius: 1.35, color: "#f00", dashed: true },
-
-        // Bottom-left
-        { x: -.975, y: -1.7, radius: 1.35, color: "#000", dashed: false },
-        { x: -.875, y: -1.65, radius: 1.35, color: "#f00", dashed: true },
-
-        // Bottom-right
-        { x: .975, y: -1.7, radius: 1.35, color: "#000", dashed: false },
-        { x: .875, y: -1.65, radius: 1.35, color: "#f00", dashed: true },
-
-        // Center
-        { x: 0, y: 0, radius: 1.35, color: "#000", dashed: false },
-        { x: 0, y: -.125, radius: 1.35, color: "#000", dashed: true }
-    ];
+    private jsonInput: string | null;
+    private controlSize: number;
+    private axisLimit: number;
 
     constructor() {
         //Empty
@@ -95,13 +74,35 @@ export class DemoField implements ComponentFramework.StandardControl<IInputs, IO
         // Add code to update control view
         this.context = context;
 
-        this.setCanvasSize();
+        const size = this.getControlSize();
+        const axisLimit = this.getAxisLimit();
+        const json = this.getJSON();
 
-        this.drawGrid();
+        let shouldRedrawGrid = false;
 
-        for (const circle of this.circles) {
-            this.drawCircle(circle)
+        if (this.controlSize !== size) {
+            this.controlSize = size;
+            this.setCanvasSize(size);
+            shouldRedrawGrid = true;
         }
+
+        if (this.axisLimit !== axisLimit) {
+            this.axisLimit = axisLimit;
+            shouldRedrawGrid = true;
+        }
+
+        if (this.jsonInput !== json) {
+            this.jsonInput = json;
+        }
+
+        if (shouldRedrawGrid) {
+            this.clearGridArea();
+            this.drawGrid();
+        }
+
+        const circles = this.getCirclesFromJSON(json);
+        this.clearCircleArea();
+        this.drawWithClipping(() => this.drawCircles(circles));
     }
 
     /**
@@ -121,29 +122,42 @@ export class DemoField implements ComponentFramework.StandardControl<IInputs, IO
     }
 
     private initializeCanvas(): void {
-        this.canvas = document.createElement("canvas");
-        this.canvas.style.border = "1px solid #000";
-        this.container.appendChild(this.canvas);
-        this.ctx = this.canvas.getContext("2d")!;
+        this.gridCanvas = document.createElement("canvas");
+        Object.assign(this.gridCanvas.style, {
+            position: "absolute",
+            border: "1px solid #000",
+            x: "0",
+            y: "0",
+            zIndex: -1
+        });
+        this.container.style.position = "relative"
+        this.container.appendChild(this.gridCanvas);
+        this.gridCtx = this.gridCanvas.getContext("2d")!;
 
-        this.setCanvasSize();
+        this.circleCanvas = document.createElement("canvas");
+        Object.assign(this.gridCanvas.style, {
+            position: "absolute",
+            border: "1px solid #000",
+            x: "0",
+            y: "0",
+            zIndex: 0
+        });
+        this.container.appendChild(this.circleCanvas);
+        this.circleCtx = this.circleCanvas.getContext("2d")!;
     }
 
-    private getCanvasSize(): { width: number, height: number } {
-        const rawWidth = this.context.parameters.controlWidth.raw || this.DEFAULT_CANVAS_SIZE;
-        const rawHeight = this.context.parameters.controlHeight.raw || this.DEFAULT_CANVAS_SIZE;
+    private getControlSize(): number {
+        const rawSize = this.context.parameters.controlSize.raw || this.DEFAULT_CANVAS_SIZE;
+        const size = Math.max(rawSize, this.MIN_CANVAS_SIZE);
 
-        const width = Math.max(rawWidth, this.MIN_CANVAS_SIZE);
-        const height = Math.max(rawHeight, this.MIN_CANVAS_SIZE);
-
-        return { width, height };
+        return size;
     }
 
-    private setCanvasSize(): void {
-        const { width, height } = this.getCanvasSize();
-
-        this.canvas.width = width;
-        this.canvas.height = height;
+    private setCanvasSize(size: number): void {
+        this.gridCanvas.width = size;
+        this.gridCanvas.height = size;
+        this.circleCanvas.width = size;
+        this.circleCanvas.height = size;
     }
 
     private initializeTooltip(): void {
@@ -156,6 +170,7 @@ export class DemoField implements ComponentFramework.StandardControl<IInputs, IO
             padding: "5px",
             borderRadius: "4px",
             fontSize: "12px",
+            whiteSpace: "nowrap",
             pointerEvents: "none",
             display: "none"
         });
@@ -164,8 +179,8 @@ export class DemoField implements ComponentFramework.StandardControl<IInputs, IO
     }
 
     private attachCanvasClickHandler(): void {
-        this.canvas.addEventListener("click", (event: MouseEvent) => {
-            const rect = this.canvas.getBoundingClientRect();
+        this.gridCanvas.addEventListener("click", (event: MouseEvent) => {
+            const rect = this.gridCanvas.getBoundingClientRect();
             const x = event.clientX - rect.left;
             const y = event.clientY - rect.top;
 
@@ -177,7 +192,7 @@ export class DemoField implements ComponentFramework.StandardControl<IInputs, IO
             const coords = this.convertPixelToGrid(x, y);
             const coordText = `(x: ${coords.x.toFixed(2)}, y: ${coords.y.toFixed(2)})`;
 
-            this.showTooltip(event.clientX + 10, event.clientY + 10, coordText);
+            this.showTooltip(x + 10, y + 10, coordText);
             this.addTooltipHideOnMouseMove();
         });
     }
@@ -202,182 +217,221 @@ export class DemoField implements ComponentFramework.StandardControl<IInputs, IO
     private addTooltipHideOnMouseMove() {
         const onMouseMove = () => {
             this.tooltip.style.display = "none";
-            this.canvas.removeEventListener("mousemove", onMouseMove);
+            this.gridCanvas.removeEventListener("mousemove", onMouseMove);
         };
 
-        this.canvas.addEventListener("mousemove", onMouseMove);
+        this.gridCanvas.addEventListener("mousemove", onMouseMove);
+    }
+
+    private getAxisLimit(): number {
+        return this.context.parameters.axisLimit.raw || this.DEFAULT_AXIS_LIMIT;
     }
 
     private getGridMetrics(): GridMetrics {
         const margin = 30;
-        const axisLimitX = this.context.parameters.axisLimitX.raw || 1;
-        const axisLimitY = this.context.parameters.axisLimitY.raw || 1;
-        const canvasWidth = this.canvas.width;
-        const canvasHeight = this.canvas.height;
+        const canvasSize = this.gridCanvas.width;
+        const axisLimit = this.axisLimit;
 
         // Compute usable area for grid
-        const usableWidth = canvasWidth - 2 * margin;
-        const usableHeight = canvasHeight - 2 * margin;
+        const usableSize = canvasSize - 2 * margin;
 
         // Calculate how much space one unit can occupy
-        const unitSizeX = usableWidth / (axisLimitX * 2); // maxX on both sides
-        const unitSizeY = usableHeight / (axisLimitY * 2); // maxY on both sides
+        const unitSizeRaw = usableSize / (axisLimit * 2);
 
         // Use the smaller to ensure it fits both directions
-        const unitSize = Math.floor(Math.min(unitSizeX, unitSizeY) / 10) * 10
+        const unitSize = Math.max(Math.floor(unitSizeRaw / 10) * 10, 10)
 
-        const gridWidth = unitSize * axisLimitX * 2;
-        const gridHeight = unitSize * axisLimitY * 2;
+        const gridSize = unitSize * axisLimit * 2;
 
-        const gridLeft = (canvasWidth - gridWidth) * .5;
-        const gridTop = (canvasHeight - gridHeight) * .5;
-        const gridRight = gridLeft + gridWidth;
-        const gridBottom = gridTop + gridHeight;
-        const centerX = gridLeft + gridWidth * .5;
-        const centerY = gridTop + gridHeight * .5;
+        const gridStart = (canvasSize - gridSize) * .5;
+        const gridEnd = gridStart + gridSize;
+        const center = gridStart + gridSize * .5;
 
         const gridStep = unitSize * .1;
 
         return {
-            gridLeft, gridTop, gridRight, gridBottom,
-            gridWidth, gridHeight,
-            centerX, centerY,
+            gridStart, gridEnd,
+            gridSize,
+            center,
             unitSize, gridStep,
-            axisLimitX, axisLimitY,
+            axisLimit,
             margin
         };
     }
 
     private isInsideGrid(x: number, y: number): boolean {
-        const { margin, gridRight, gridBottom } = this.getGridMetrics();
+        const { margin, gridEnd } = this.getGridMetrics();
         return (
             x >= margin &&
-            x <= gridRight &&
+            x <= gridEnd &&
             y >= margin &&
-            y <= gridBottom
+            y <= gridEnd
         );
     }
 
     private convertPixelToGrid(pixelX: number, pixelY: number): { x: number; y: number } {
-        const { centerX, centerY, unitSize } = this.getGridMetrics();
+        const { center, unitSize } = this.getGridMetrics();
 
-        const gridX = (pixelX - centerX) / unitSize;
-        const gridY = (centerY - pixelY) / unitSize;
+        const gridX = (pixelX - center) / unitSize;
+        const gridY = (center - pixelY) / unitSize;
 
         return { x: gridX, y: gridY };
     }
 
     private convertGridToPixel(gridX: number, gridY: number): { x: number; y: number } {
-        const { centerX, centerY, unitSize } = this.getGridMetrics();
+        const { center, unitSize } = this.getGridMetrics();
 
-        const x = centerX + gridX * unitSize;
-        const y = centerY - gridY * unitSize;
+        const x = center + gridX * unitSize;
+        const y = center - gridY * unitSize;
 
         return { x, y };
     }
 
+    private clearGridArea(): void {
+        this.gridCtx.clearRect(0, 0, this.gridCanvas.width, this.gridCanvas.height);
+    }
+
+    private clearCircleArea(): void {
+        this.circleCtx.clearRect(0, 0, this.circleCanvas.width, this.circleCanvas.height);
+    }
+
+
     private drawGrid(): void {
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.drawGridLines();
         this.drawAxes();
         this.drawLabels();
     }
 
     private drawGridLines(): void {
-        const { gridLeft, gridRight, gridTop, gridBottom, gridWidth, gridHeight, unitSize, gridStep } = this.getGridMetrics();
+        const { gridStart, gridEnd, gridSize, unitSize, gridStep } = this.getGridMetrics();
 
         const halfUnitSize = unitSize * .5;
 
+        this.gridCtx.lineWidth = 1;
+
         let xPos = 0;
-        for (let x = 0; x <= gridWidth; x += gridStep) {
-            xPos = gridLeft + x + 0.5;
-            this.ctx.beginPath();
-            this.ctx.setLineDash(x % halfUnitSize === 0 ? [] : [1, 1]);
-            this.ctx.moveTo(xPos, gridTop);
-            this.ctx.lineTo(xPos, gridBottom);
-            this.ctx.stroke();
+        for (let x = 0; x <= gridSize; x += gridStep) {
+            xPos = gridStart + x + .5;
+            this.gridCtx.beginPath();
+            this.gridCtx.setLineDash(x % halfUnitSize === 0 ? [] : [1, 1]);
+            this.gridCtx.moveTo(xPos, gridStart);
+            this.gridCtx.lineTo(xPos, gridEnd);
+            this.gridCtx.stroke();
         }
 
         let yPos = 0;
-        for (let y = 0; y <= gridHeight; y += gridStep) {
-            yPos = gridTop + y + 0.5;
-            this.ctx.beginPath();
-            this.ctx.setLineDash(y % halfUnitSize === 0 ? [] : [1, 1]);
-            this.ctx.moveTo(gridLeft, yPos);
-            this.ctx.lineTo(gridRight, yPos);
-            this.ctx.stroke();
+        for (let y = 0; y <= gridSize; y += gridStep) {
+            yPos = gridStart + y + .5;
+            this.gridCtx.beginPath();
+            this.gridCtx.setLineDash(y % halfUnitSize === 0 ? [] : [1, 1]);
+            this.gridCtx.moveTo(gridStart, yPos);
+            this.gridCtx.lineTo(gridEnd, yPos);
+            this.gridCtx.stroke();
         }
     }
 
     private drawAxes(): void {
-        const { gridLeft, gridRight, gridTop, gridBottom, centerX, centerY } = this.getGridMetrics();
+        const { gridStart, gridEnd, center } = this.getGridMetrics();
 
-        this.ctx.setLineDash([]);
-        this.ctx.strokeStyle = "#000";
-        this.ctx.lineWidth = 2;
+        this.gridCtx.setLineDash([]);
+        this.gridCtx.strokeStyle = "#000";
+        this.gridCtx.lineWidth = 2;
 
-        this.ctx.beginPath();
-        this.ctx.moveTo(gridLeft, centerY);
-        this.ctx.lineTo(gridRight, centerY);
-        this.ctx.stroke();
+        this.gridCtx.beginPath();
+        this.gridCtx.moveTo(gridStart, center);
+        this.gridCtx.lineTo(gridEnd, center);
+        this.gridCtx.stroke();
 
-        this.ctx.beginPath();
-        this.ctx.moveTo(centerX, gridBottom);
-        this.ctx.lineTo(centerX, gridTop);
-        this.ctx.stroke();
+        this.gridCtx.beginPath();
+        this.gridCtx.moveTo(center, gridEnd);
+        this.gridCtx.lineTo(center, gridStart);
+        this.gridCtx.stroke();
     }
 
     private drawLabels(): void {
-        const { centerX, centerY, gridTop, gridRight, unitSize, axisLimitX, axisLimitY } = this.getGridMetrics();
+        const { center, gridStart, gridEnd, unitSize, axisLimit } = this.getGridMetrics();
 
         const fontSize = 10;
-        this.ctx.font = `${fontSize}px sans-serif`;
-        this.ctx.textAlign = "left";
-        this.ctx.textBaseline = "middle";
-        this.ctx.fillStyle = "#000";
+        this.gridCtx.font = `${fontSize}px sans-serif`;
+        this.gridCtx.textAlign = "left";
+        this.gridCtx.textBaseline = "middle";
+        this.gridCtx.fillStyle = "#000";
 
         // X-axis labels
-        for (let i = -axisLimitX; i <= axisLimitX; i += .5) {
-            const x = centerX + i * unitSize;
-            this.ctx.save();
-            this.ctx.translate(x, gridTop - 5);
-            this.ctx.rotate(-Math.PI * .5);
-            this.ctx.fillText(i.toFixed(2), 0, 0);
-            this.ctx.restore();
+        for (let i = -axisLimit; i <= axisLimit; i += .5) {
+            const x = center + i * unitSize;
+            this.gridCtx.save();
+            this.gridCtx.translate(x, gridStart - 5);
+            this.gridCtx.rotate(-Math.PI * .5);
+            this.gridCtx.fillText(i.toFixed(2), 0, 0);
+            this.gridCtx.restore();
         }
 
         // Y-axis labels
-        for (let j = -axisLimitY; j <= axisLimitY; j += 0.5) {
-            const y = centerY - j * unitSize;
-            this.ctx.fillText(j.toFixed(2), gridRight + 5, y);
+        for (let j = -axisLimit; j <= axisLimit; j += 0.5) {
+            const y = center - j * unitSize;
+            this.gridCtx.fillText(j.toFixed(2), gridEnd + 5, y);
         }
     }
 
+    private drawWithClipping(drawFn: () => void) {
+        const { gridStart, gridSize } = this.getGridMetrics();
+
+        this.circleCtx.save();
+        this.circleCtx.beginPath();
+        this.circleCtx.rect(gridStart, gridStart, gridSize, gridSize);
+        this.circleCtx.clip();
+
+        drawFn();
+
+        this.circleCtx.restore();
+    }
+
     private drawCircle(circle: Circle): void {
-        const { x, y, radius, color, dashed } = circle;
+        const { x, y, radius, outlineColor, fillColor, dashed } = circle;
         const { unitSize } = this.getGridMetrics();
+        const { circleCtx: ctx } = this;
 
         const { x: canvasX, y: canvasY } = this.convertGridToPixel(x, y);
 
         //outer circle
-        this.ctx.beginPath();
-        this.ctx.setLineDash(dashed ? [5, 3] : []);
-        this.ctx.strokeStyle = color;
-        this.ctx.lineWidth = 2;
-        this.ctx.arc(canvasX, canvasY, radius * unitSize, 0, Math.PI * 2);
-        this.ctx.stroke();
+        ctx.beginPath();
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = outlineColor;
+        ctx.setLineDash(dashed ? [5, 3] : []);
+        ctx.arc(canvasX, canvasY, radius * unitSize, 0, Math.PI * 2);
 
-        //inner circle
-        this.ctx.beginPath();
-        this.ctx.strokeStyle = this.INNER_CIRCLE_OUTLINE;
-        this.ctx.setLineDash(dashed ? [6, 4] : []);
-        this.ctx.arc(canvasX, canvasY, .06 * radius * unitSize, 0, Math.PI * 2);
+        if (fillColor) {
+            ctx.fillStyle = fillColor;
+            ctx.fill();
 
-        if (!dashed) {
-            this.ctx.fillStyle = this.INNER_CIRCLE_FILL;
-            this.ctx.fill();
+        }
+        ctx.stroke();
+    }
+
+    private drawCircles(circles: Circle[]) {
+        const sortedCircles = [...circles].sort((a, b) => a.zIndex - b.zIndex);
+
+        for (const circle of sortedCircles) {
+            this.drawCircle(circle)
+        }
+    }
+
+    private getJSON(): string | null {
+        return this.context.parameters.jsonInput.raw;
+    }
+
+    private getCirclesFromJSON(json: string | null): Circle[] {
+        if (!json) {
+            return [];
         }
 
-        this.ctx.stroke();
+        try {
+            const parsed: Circle[] = JSON.parse(json);
+            return parsed;
+        } catch (e) {
+            console.error("Invalid JSON in circleData:", e);
+            return [];
+        }
     }
 }
